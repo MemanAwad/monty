5de1d2758e54db956b9bdcb656f38a066a03c7d9
Eilla-7 <eilafadel2238@gmail.com>
2023-11-16 12:56:26 -0800

    file handling

---
+++ process_file.c
@@ -0,0 +1,74 @@
+#include "monty.h"
+#include <stdio.h>
+
+/**
+ *  readfile - function to read the content of monty file
+ *  @file: the monty file
+ *  @stack: the stack that we will perform the operations in
+ *
+ *  Return: 0 if success 1 if not
+ */
+int readfile(char *file, stack_t **stack)
+{
+	char buffer[MAX];
+	int fd = 0;
+	FILE *content;
+	int line_number = 1;
+	func ob_func;
+
+	content = fopen(file, "r");
+	if (content == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", file);
+		free_stack(stack);
+		exit(EXIT_FAILURE);
+	}
+	while (fgets(buffer, sizeof(buffer), content) != NULL)
+	{
+		ob_func = get_function(buffer);
+		if (ob_func == NULL)
+		{
+			fprintf(stderr, "L%d: unknown instruction %s\n", line_number, buffer);
+			free_stack(stack);
+			exit(EXIT_FAILURE);
+		}
+		ob_func(stack, line_number);
+		line_number++;
+	}
+	fd = fclose(content);
+	if (fd == -1)
+	{
+		free_stack(stack);
+		exit(EXIT_FAILURE);
+	}
+	return (0);
+}
+/**
+ * get_function - function to return the function that is responsible of
+ * doing the specified stack operation
+ * @buffer: buffer hold the operation
+ *
+ * Return: a function pointer to the operation or NULL
+ */
+func get_function(char *buffer)
+{
+	int i = 0;
+	char *delim = " \n";
+	char *token = strtok(buffer, delim);
+
+	instruction_t ins[] = {
+		{"push", _push},
+		{"pall", _pall},
+		{"pint", _pint},
+		{"pop", _pop},
+		{"swap", _swap},
+		{"add", _add},
+		{"nop", _nop},
+		{NULL, NULL}
+	};
+	while (ins[i].opcode != NULL && strcmp(ins[i].opcode, token) != 0)
+	{
+		i++;
+	}
+	return (ins[i].f);
+}
