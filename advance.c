7688217dc38ae491c080859d84547ed9a7e014b0
MemanAwad <meman1awad@gmail.com>
2023-11-17 01:55:04 +0200

    advance

--- advance.c
+++ advance.c
@@ -26,3 +26,61 @@ void _sub(stack_t **stack, unsigned int line_number)
 
 }
 
+/**
+ * _div - a function that divide last node in the stack to node above
+ * @stack: the stack
+ * @line_number: the number of spacific line
+ *
+ * Return: void
+ */
+
+void _div(stack_t **stack, unsigned int line_number)
+{
+	int div = 0;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
+		free_stack(stack);
+		exit(EXIT_FAILURE);
+	}
+	(*stack) = (*stack)->next;
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", line_number);
+		free_stack(stack);
+		exit(EXIT_FAILURE);
+
+	}
+	div = (*stack)->n / (*stack)->prev->n;
+	(*stack)->n = div;
+	free((*stack)->prev);
+	(*stack)->prev = NULL;
+
+}
+/**
+ * _mul - a function that substract last node in the stack from node above
+ * @stack: the stack
+ * @line_number: the number of spacific line
+ *
+ * Return: void
+ */
+
+void _mul(stack_t **stack, unsigned int line_number)
+{
+	int mul = 0;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
+		free_stack(stack);
+		exit(EXIT_FAILURE);
+	}
+	(*stack) = (*stack)->next;
+	mul = (*stack)->n * (*stack)->prev->n;
+	(*stack)->n = mul;
+	free((*stack)->prev);
+	(*stack)->prev = NULL;
+
+}
+
